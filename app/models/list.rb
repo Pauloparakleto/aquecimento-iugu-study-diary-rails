d956312e67293c48b8e2fb1a0d9fc45925d09121
Paulo Felipe <alparakleto@gmail.com>
2021-03-07 20:10:13 -0300

    Normalize params name and category for list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,18 @@
 class List < ApplicationRecord
     validates :name, presence: true
-    validates :description, presence: true, length: { minimum: 10, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 5, maximum: 100 }
+    before_validation :normalize_params, on: :create
+
+    private
+    def normalize_params
+        normalize_name
+        normalize_category
+    end
+    def normalize_name
+      self.name = name.downcase
+    end
+
+    def normalize_category
+        self.category = category.downcase
+      end
 end
