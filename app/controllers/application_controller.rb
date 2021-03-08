48e515816b54b3f9cca9a76fe72254f26f32cc84
Paulo Felipe <alparakleto@gmail.com>
2021-03-08 16:45:38 -0300

    Fix ransack warning deprection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_search
-    @q = List.search(params[:q])
+    @q = List.ransack(params[:q])
   end
   
     protected
