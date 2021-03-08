b1afdcd83cad299d7b135098a8433447ab2e61ca
Paulo Felipe <alparakleto@gmail.com>
2021-03-08 13:59:45 -0300

    Fix bug no ransack params was passed to form for search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
- 
+
+  def set_search
+    @q = List.search(params[:q])
+  end
+  
     protected
     
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def welcome
-    #@q = List.ransack(params[:q])
-    #@lists = @q.result(distinct: true)
+    @q = List.ransack(params[:q])
+    @lists = @q.result(distinct: true)
     #@lists = List.all
   end
 end
