4aa0bd2a0e150585affc338cc90094188f1d4aaa
Paulo Felipe <alparakleto@gmail.com>
2021-03-08 16:22:42 -0300

    Add tests for list controller

--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -1,4 +1,67 @@
 require "test_helper"
 
 class ListsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "Should not create list with not logged user" do
+    get lists_new_url
+    assert_response :redirect
+
+    post "/lists",
+    params: { list: { name: "", category: "Category_name"} }
+    assert_response :redirect      
+   end
+
+  test "Should not create list with empity name" do
+    sign_in users(:one)
+    get lists_new_url
+    assert_response :success
+
+    post "/lists",
+    params: { list: { name: "", category: "Category_name"} }
+    assert_response :success      
+   end
+
+   test "Should not create list with minimum name not filled" do
+    sign_in users(:one)
+    get lists_new_url
+    assert_response :success
+
+    post "/lists",
+    params: { list: { name: "a", category: "Category_name"} }
+    assert_response :success      
+   end
+
+   test "Should not create list with empty description not filled" do
+    sign_in users(:one)
+    get lists_new_url
+    assert_response :success
+
+    post "/lists",
+    params: { list: { name: "Valid name", description: "", category: "Category_name"} }
+    assert_response :success      
+   end
+
+   test "Should not create list with empty category" do
+    sign_in users(:one)
+    get lists_new_url
+    assert_response :success
+
+    post "/lists",
+    params: { list: { name: "Valid name", description: "", category: ""} }
+    assert_response :success      
+   end
+
+   test "Should create list with valid params" do
+    sign_in users(:one)
+    get lists_new_url
+    assert_response :success
+
+    post "/lists",
+    params: { list: { name: "Ruby", description: "This is the description",
+              category: "Category_name"} }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success      
+   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,30 +1,4 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-=begin
-  test "Not logged user should redirect on try get users index" do
-
-    get list_url
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
-  end
-
-
-  test "Admin logged should get users index" do
-    sign_in users(:one)
-
-    get users_url
-    assert_response :success
-  end
-
-  test "Non admin user logged in should redirect on try get users index" do
-    sign_in users(:two)
-
-    get users_url
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
-  end
-=end
 end
